0226f500d5804cde1ab1aeb44faeb825ce9b9db3
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-15 00:03:51 -0700

    commit all

--- fileio64/Program.cs
+++ fileio64/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace fileio64
 {
@@ -10,6 +11,13 @@ namespace fileio64
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Desktop\number.txt", input);
+            string fileRead = File.ReadAllText(@"C:\Users\Student\Desktop\number.txt");
+            Console.WriteLine("Here is what you're file contains: " + fileRead);
+            Console.ReadLine();
+
         }
     }
 }
